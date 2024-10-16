7eb15cd121215e3299ce0a5a5a2c9c3f67205572
woony-nam <saki.s2.minami@gmail.com>
2024-10-16 11:40:04 +0900

    sign upとloginまわりの調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_15_144128) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_16_022751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_15_144128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
