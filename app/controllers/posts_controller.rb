f14ca357301d4e8990dd4cf838f0248db65ae73e
woony-nam <saki.s2.minami@gmail.com>
2024-10-17 23:39:14 +0900

    UI使ってみた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
