e00a2345f06d2fccc922d30002035b816073a4d9
woony-nam <saki.s2.minami@gmail.com>
2024-10-17 12:54:15 +0900

    Renderエラーを解消する

--- db/migrate/20241015044628_add_name_to_users.rb
+++ db/migrate/20241015044628_add_name_to_users.rb
@@ -1,5 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :username, :string, null: false, unique: true
+    add_column :users, :username, :string, null: false
+    add_index :users, :username, unique: true
   end
 end
